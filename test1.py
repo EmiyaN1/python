faa7c06b7c7cd4a011578d9946d7862d7601b81b
ShiRo <1151637747@qq.com>
2022-03-15 18:41:19 +0800

    try

--- test1.py
+++ test1.py
@@ -1,1 +1,11 @@
-#######
+num = int(input("输入分子："))
+den = int(input("输入分母："))
+def gcd(num,den):
+    if num%den==0:
+        num=num/den
+        den = den / den
+    else:
+        num = num
+        den = den
+    return print(num,"/",den)
+gcd(num,den)
